27c48175d3f199f334b953e8f6bdba7a11b8fc23
Silverclean091 <wjdghkdidi@naver.com>
2023-09-25 20:47:28 +0900

    MV n to d

---
+++ Week.2-1(230919)/Chapter10-02.js
@@ -0,0 +1,15 @@
+var wind = {
+    'last-name' : 'park',
+    1: 10
+};
+
+// wind.'last-name';
+// wind.last-name;
+
+// wind[last-name];
+wind['last-name'];
+
+// wind.1;
+// wind.'1';
+wind[1];
+wind['1'];
